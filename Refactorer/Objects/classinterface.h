84b5f89a8f5a366a30dcc21ca884b6c86d3c57b7
WezSieTato <le0n.uo@gmail.com>
2014-05-24 11:16:05 +0200

    classinterface

---
+++ Refactorer/Objects/classinterface.h
@@ -0,0 +1,42 @@
+#ifndef CLASSINTERFACE_H
+#define CLASSINTERFACE_H
+
+#include "propertydeclaration.h"
+#include "methoddeclaration.h"
+#include "classpart.h"
+
+namespace objc {
+
+typedef std::list< VariableDeclaration > VariableDeclarationList;
+typedef std::list< PropertyDeclaration > PropertyDeclarationList;
+typedef std::list< MethodDeclaration > MethodDeclarationList;
+
+
+class ClassInterface : public ClassPart
+{
+public:
+    ClassInterface();
+
+    std::string baseClass() const;
+    void setBaseClass(const std::string &baseClass);
+
+    VariableDeclarationList variableDeclarations() const;
+    void setVariableDeclarations(const VariableDeclarationList &variableDeclarations);
+
+    PropertyDeclarationList propertyDeclarations() const;
+    void setPropertyDeclarations(const PropertyDeclarationList &propertyDeclarations);
+
+    MethodDeclarationList methodDeclarations() const;
+    void setMethodDeclarations(const MethodDeclarationList &methodDeclarations);
+
+private:
+
+    std::string _baseClass;
+
+    VariableDeclarationList _variableDeclarations;
+    PropertyDeclarationList _propertyDeclarations;
+    MethodDeclarationList _methodDeclarations;
+};
+
+}
+#endif // CLASSINTERFACE_H
